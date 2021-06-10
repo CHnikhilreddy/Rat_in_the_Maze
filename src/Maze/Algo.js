4b8fd802b0411a21d8fb4661d0d3cd4f7b5caac0
chamakura nikhil reddy <chamakuranikhilreddy@gmail.com>
2021-06-10 11:41:29 +0530

    added all direction

--- src/Maze/Algo.js
+++ src/Maze/Algo.js
@@ -4,35 +4,51 @@ import './maze.css';
 
 function Algo (){
     const [displayAns,setDisplayAns] = useState(false);
+    const size = 8;
     var arr = [
-        [3,1,1,1],
-        [0,1,0,1],
-        [0,1,1,1],
-        [1,0,0,2]   
+        [3,0,1,1,0,1,1,1],
+        [1,1,0,1,1,1,0,1],
+        [1,1,1,1,0,0,1,1],
+        [1,0,0,0,1,0,1,0],
+        [1,1,1,0,1,1,1,0],
+        [0,0,1,0,1,0,0,0],
+        [1,0,1,0,1,1,0,0],
+        [1,1,1,0,1,1,1,2]   
     ];
     var solution_demo = arr.map(function(a) {
         return a.slice();
     });
     var solution = [];
     function ans_demo(arr_qs,arr_ans,row,col){
-        if(row===3 && col === 3){
+        if(row===(size-1) && col === (size-1)){
             arr_ans[row][col] = 2;
             solution.push(arr_ans);
             return;
         }
-        if((row+1)!==4 && arr_qs[row+1][col] !== 0){
+        if((row+1)!==size && arr_qs[row+1][col] !== 0 && arr_ans[row+1][col] !== 5){
             let arr_new = arr_ans.map((a)=>{return a.slice();})
             arr_new[row+1][col] = 5;
             ans_demo(arr_qs,arr_new,row+1,col)
         }
-        if((col+1)!==4 && arr_qs[row][col+1] !== 0){
+        if((col+1)!==size && arr_qs[row][col+1] !== 0 && arr_ans[row][col+1] !== 5){
             let arr_new = arr_ans.map((a)=>{return a.slice();})
             arr_new[row][col+1] = 5;
             ans_demo(arr_qs,arr_new,row,col+1)
         }
+        if((col-1)!==-1 && arr_qs[row][col-1] !== 0 && arr_ans[row][col-1] !== 5){
+            let arr_new = arr_ans.map((a)=>{return a.slice();})
+            arr_new[row][col-1] = 5;
+            ans_demo(arr_qs,arr_new,row,col-1)
+        }
+        if((row-1)!==-1 && arr_qs[row-1][col] !== 0 && arr_ans[row-1][col] !== 5){
+            let arr_new = arr_ans.map((a)=>{return a.slice();})
+            arr_new[row-1][col] = 5;
+            ans_demo(arr_qs,arr_new,row-1,col)
+        }
         
     }
     ans_demo(arr,solution_demo,0,0);
+    console.log(arr)
 
     return (<>
     <Maze displayMiddle = {5} maze={arr}/>
